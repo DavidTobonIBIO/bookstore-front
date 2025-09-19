004f2cc8dda7cd41c836b46457c23a26aa86bb02
DavidTobonIBIO <d.tobonm2@uniandes.edu.co>
2025-09-19 16:10:25 -0500

    add author button

--- src/app/authors/page.tsx
+++ src/app/authors/page.tsx
@@ -24,36 +24,38 @@ export default function Authors() {
     return <div className="p-4 text-red-500">Error: {error}</div>;
   }
 
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold">Authors List</h1>
-      <Link
-        className="px-3 font-bold hover:text-gray-300"
-        href="/authors/create"
-      >
-        Create a new Author
-      </Link>
-
-      {authors.length === 0 ? (
-        <p>There are no authors.</p>
-      ) : (
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {authors.map((author) => (
-            <div key={author.id} className="border rounded-lg p-4">
-              <Image
-                width={500}
-                height={500}
-                src={author.image}
-                alt={author.name}
-                className="w-full h-100 object-cover rounded mb-4"
-              />
-              <h2 className="text-xl font-bold">{author.name}</h2>
-              <p className="text font-bold">Born: {author.birthDate}</p>
-              <p className="text font-bold">{author.description}</p>
+    return (
+        <div className="container mx-auto p-4">
+            <div className="flex justify-between items-center mb-6">
+            <h1 className="text-4xl font-bold">Authors List</h1>
+            <Link
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition-colors duration-200"
+                href="/authors/create"
+            >
+                Add Author
+            </Link>
             </div>
-          ))}
+
+            {authors.length === 0 ? (
+            <p>There are no authors.</p>
+            ) : (
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+                {authors.map((author) => (
+                <div key={author.id} className="border rounded-lg p-4">
+                    <Image
+                    width={500}
+                    height={500}
+                    src={author.image}
+                    alt={author.name}
+                    className="w-full h-100 object-cover rounded mb-4"
+                    />
+                    <h2 className="text-xl font-bold">{author.name}</h2>
+                    <p className="text font-bold">Born: {author.birthDate}</p>
+                    <p className="text font-bold">{author.description}</p>
+                </div>
+                ))}
+            </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
